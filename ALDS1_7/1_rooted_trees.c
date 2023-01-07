85dcee3e60523ff14b374fd06c08c1744c9f5bda
Yuorei <yuorei71@gmail.com>
2023-01-07 14:25:55 +0900

    完成

--- ALDS1_7/1_rooted_trees.c
+++ ALDS1_7/1_rooted_trees.c
@@ -1,60 +1,80 @@
 #include <stdio.h>
 #define NIL -1
-#define MAX 100005
+#define MAX 100000
 
 typedef struct{
-    int id;
-    int node;
     int pearent;
+    int left_child;
+    int right_sibling;
     int depth;
-    int child_som;
-    int child_node_list[MAX];
-}INFOMATION;
+}NODE;
+
+NODE Q[MAX];
+int n;
 int main(){
-    INFOMATION Q[MAX];
-    
-    int n,m,tmp,id;
+    int m,num,id,l,k;
     scanf("%d",&n);
     for(int i=0;i<n;i++){
-        Q[i].pearent=-1;
+        Q[i].pearent=NIL;
+        Q[i].left_child=NIL;
+        Q[i].right_sibling=NIL;
         Q[i].depth=0;
-        Q[i].child_node_list[0]=NIL;
     }
     for(int i=0 ; i<n;i++){
         scanf("%d%d",&id,&m);
-        Q[id].id=id;
-        Q[id].child_som=m;
         for(int j=0;j<m;j++){
-            scanf("%d",&tmp);
-            Q[id].child_node_list[j]=tmp;
-            Q[Q[id].child_node_list[j]].pearent=Q[id].id;
+            scanf("%d",&num);
+            if(j==0){
+                Q[id].left_child=num;
+            }
+            else{
+                Q[l].right_sibling=num;
+            }
+            l=num;
+            Q[num].pearent=id;
         }
     }
-    
+
     for(int i=0;i<n;i++){
         m=i;
-        while(Q[m].pearent!=-1){
+        while(Q[m].pearent!=NIL){
             m=Q[m].pearent;
             Q[i].depth++;
         }
-        if(Q[i].pearent==-1){
-            printf("node %d: parent = %d, depth = %d, root, [",Q[i].id,Q[i].pearent,Q[i].depth);
-            if(Q[i].child_node_list[0]!=NIL){
-                printf("%d",Q[i].child_node_list[0]);
-            }
-            for(int j=1;j<Q[i].child_som;j++){
-                printf(", %d",Q[i].child_node_list[j]);
+        if(Q[i].pearent==NIL){
+            printf("node %d: parent = %d, depth = %d, root, [",i,Q[i].pearent,Q[i].depth);
+            l=Q[i].left_child;
+            k=0;
+            while (l!=NIL)
+            {
+                if(k==0){
+                    printf("%d",l);
+                    k++;
+                }
+                else{
+                    printf(", %d",l);
+                }
+                l=Q[l].right_sibling;
             }
             printf("]\n");
         }
-        else if(Q[i].child_node_list[0]==NIL){
-            printf("node %d: parent = %d, depth = %d, leaf, []\n",Q[i].id,Q[i].pearent,Q[i].depth);
+        else if(Q[i].left_child==NIL){
+            printf("node %d: parent = %d, depth = %d, leaf, []\n",i,Q[i].pearent,Q[i].depth);
         }
         else {
-            printf("node %d: parent = %d, depth = %d, internal node, [",Q[i].id,Q[i].pearent,Q[i].depth);
-            printf("%d",Q[i].child_node_list[0]);
-            for(int j=1;j<Q[i].child_som;j++){
-                printf(", %d",Q[i].child_node_list[j]);
+            printf("node %d: parent = %d, depth = %d, internal node, [",i,Q[i].pearent,Q[i].depth);
+            l=Q[i].left_child;
+            k=0;
+            while (l!=NIL)
+            {
+                if(k==0){
+                    printf("%d",l);
+                    k++;
+                }
+                else{
+                    printf(", %d",l);
+                }
+                l=Q[l].right_sibling;
             }
             printf("]\n");
         }
